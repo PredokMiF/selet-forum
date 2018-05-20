2ea96e020ba98a0871f2348c1b27682d318756f1
MiF <grigorchuk.konstantin@gmail.com>
2018-05-20 22:46:42 +0300

    [bf] Добавил инициализацию множества галерей

--- js/custom-gallery.js
+++ js/custom-gallery.js
@@ -1,25 +1,27 @@
 export function runCustomPreGallery() {
-    const $galeryParent = $('#galery-block')
+    $('[id="galery-block"]').each((i, galeryParent) => {
+        const $galeryParent = $(galeryParent)
 
-    const srcList = $galeryParent.find('.galery img').toArray().map(el => $(el).attr('src'))
-    const cards = srcList.map(src => `
-        <div class="carousel-card" style="background-image: url('${src}');"></div>
-    `).join('')
+        const srcList = $galeryParent.find('.galery img').toArray().map(el => $(el).attr('src'))
+        const cards = srcList.map(src => `
+            <div class="carousel-card" style="background-image: url('${src}');"></div>
+        `).join('')
 
 
-    $galeryParent.append(`
-        <div class="galery-carousel carousel-wrapper" data-cout-foo="return 1">
-            <div class="carousel-flip" data-flip="prev"></div>
-    
-            <div class="carousel-viewport">
-                <div class="hidden">
-                    ${cards}
-                </div>        
+        $galeryParent.append(`
+            <div class="galery-carousel carousel-wrapper" data-cout-foo="return 1">
+                <div class="carousel-flip" data-flip="prev"></div>
+        
+                <div class="carousel-viewport">
+                    <div class="hidden">
+                        ${cards}
+                    </div>        
+                </div>
+        
+                <div class="carousel-flip" data-flip="next"><div class="carousel-flip-inner"></div></div>
             </div>
-    
-            <div class="carousel-flip" data-flip="next"><div class="carousel-flip-inner"></div></div>
-        </div>
-    `)
+        `)
 
-    $galeryParent.append(`<div class="carousel-paging"></div>`)
+        $galeryParent.append(`<div class="carousel-paging"></div>`)
+    })
 }
